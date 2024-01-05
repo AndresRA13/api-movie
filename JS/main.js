ff5992f14a6c1e30aafddb3ca82d40eb966b4e16
Andres Rojas <127151298+AndresRA13@users.noreply.github.com>
2024-01-05 16:57:32 -0500

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -56,28 +56,38 @@ let pagina = 1;
       }
     });
 
-    btnBuscar.addEventListener('click', async () => {
-      const query = document.getElementById('inputBusqueda').value.toLowerCase();
-      if (query.trim() !== '') {
-        try {
-          const respuesta = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=89bd3727826c10b308ce5d4bc04a2027&language=es-MX&query=${query}&page=1`);
-          if (respuesta.status === 200) {
-            const datos = await respuesta.json();
-            mostrarPeliculas(datos.results);
-          } else {
-            console.log('Error al realizar la búsqueda');
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      } else {
-        // Si la búsqueda está vacía, cargamos las películas populares.
-        cargarPeliculas(1).then(peliculas => mostrarPeliculas(peliculas));
-      }
-    });
+ 
+      const inputBusqueda = document.getElementById('inputBusqueda');
 
-    // Llamamos a cargarPeliculas al cargar la página para obtener las películas populares.
-    cargarPeliculas(1).then(peliculas => mostrarPeliculas(peliculas));
+      inputBusqueda.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+          realizarBusqueda();
+        }
+      });
+      
+      const realizarBusqueda = async () => {
+        const query = inputBusqueda.value.toLowerCase();
+        if (query.trim() !== '') {
+          try {
+            const respuesta = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=89bd3727826c10b308ce5d4bc04a2027&language=es-MX&query=${query}&page=1`);
+            if (respuesta.status === 200) {
+              const datos = await respuesta.json();
+              mostrarPeliculas(datos.results);
+            } else {
+              console.log('Error al realizar la búsqueda');
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        } else {
+          // Si la búsqueda está vacía, cargamos las películas populares.
+          cargarPeliculas(1).then(peliculas => mostrarPeliculas(peliculas));
+        }
+      };
+      
+      // Llamamos a cargarPeliculas al cargar la página para obtener las películas populares.
+      cargarPeliculas(1).then(peliculas => mostrarPeliculas(peliculas));
+      
 /** SECTION NAV BARS */
 const openBars = document.querySelector('.open_bars');
 const closeBars = document.querySelector('.close_bars');
@@ -105,3 +115,18 @@ const body =  document.querySelector('.body');
 function night(){
     body.classList.toggle('night');
 }
+
+const inputSearch = document.getElementById('inputBusqueda');
+const btnSearch =  document.querySelector('.initial');
+const btnPend = document.querySelector('.notPend');
+btnSearch.addEventListener('click', () =>{
+    inputSearch.classList.add('active');
+    btnSearch.classList.add('active');
+    btnPend.classList.add('active');
+})
+btnPend.addEventListener('click', () =>{
+    inputSearch.classList.remove('active');
+    btnSearch.classList.remove('active');
+    btnPend.classList.remove('active');
+    btnPend.style.position = 'absolute';
+})
